ecf338e5b2ecef3d5c9818d27836d9fd02a96f1f
Gerald Ragghianti <ragghianti@icl.utk.edu>
2025-06-12 13:10:27 +0000

    Testing

---
+++ .github/workflows/env_setup.sh
@@ -0,0 +1,30 @@
+# This file should be sourced into the shell environment
+
+# Show the executed command, but don't affect spawned shells
+trap 'echo "# $BASH_COMMAND"' DEBUG # Show commands
+
+if [[ -z "$SPACK_SETUP" || ! -e "$SPACK_SETUP" ]]; then
+   echo Error! Environment variable \$SPACK_SETUP must point
+   echo to a valid setup-env.sh Spack setup script.
+   exit 1
+fi
+source $SPACK_SETUP
+spack env activate -V parsec
+spack load python
+spack load cmake
+spack load openmpi
+spack load cuda
+spack load ninja
+
+export CUDA=OFF
+export HIP=OFF
+export INTEL=OFF
+if [ "$DEVICE" = "gpu_nvidia" ]; then
+   spack load cuda
+   CUDA=ON
+elif [ "$DEVICE" = "gpu_amd" ]; then
+   HIP=ON
+elif [ "$DEVICE" = "gpu_intel" ]; then
+   INTEL=ON
+fi
+
